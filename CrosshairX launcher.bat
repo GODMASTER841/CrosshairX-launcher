21153a5d570bec0d38acb70219c9de6a338b5a03
GODMASTER841 <167480779+GODMASTER841@users.noreply.github.com>
2024-06-02 01:42:09 +0330

    Delete CrosshairX launcher.bat

--- CrosshairX launcher.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-start /b "GameBar.exe" "C:\Program Files\WindowsApps\Microsoft.XboxGamingOverlay_7.124.5142.0_x64__8wekyb3d8bbwe\GameBar.exe"
-timeout /t 1
-nircmd win min process "GameBar.exe"
-@echo off
-start /b "CrosshairX.exe" "C:\Program Files (x86)\Steam\steamapps\common\CrosshairX\CrosshairX.exe"
-timeout /t 8
-taskkill /f /im CrosshairX.exe
